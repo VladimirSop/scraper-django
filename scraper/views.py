555b3761174be87ae64c80cc5fea4f87d9c4f229
VladimirSop <vladimir.sop.sw@gmail.com>
2026-05-07 18:55:45 +0300

    Add docker support

--- scraper/views.py
+++ scraper/views.py
@@ -6,18 +6,28 @@ def index(request):
     return render(request, 'index.html')
 
 def product_list(request):
-    query = request.GET.get("q", "")
+    query = request.GET.get("q", "").strip()
+    sort = request.GET.get("sort", "")
 
     products = Product.objects.all()
 
     if query:
         products = products.filter(name__icontains=query)
 
-    paginator = Paginator(products, 25)
+    # Sorting
+    if sort == "price_asc":
+        products = products.order_by("price")
+    elif sort == "price_desc":
+        products = products.order_by("-price")
+    elif sort == "name":
+        products = products.order_by("name")
+
+    paginator = Paginator(products, 24)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
     return render(request, "scraper/index.html", {
         "page_obj": page_obj,
-        "query": query
+        "query": query,
+        "sort": sort
     })
